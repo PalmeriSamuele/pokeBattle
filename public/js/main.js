f0c3f6e71f66502c236aff5ec0ded2721dc78454
sam <samuele2231@hotmail.com>
2022-07-05 14:18:21 +0200

    improve game logic

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,38 @@
-import {attaque } from "./modules/pokemons.js";
+import {attaque, attaque_ennemies, narr_box } from "./modules/pokemons.js";
+// on recupere les attaques
 let btn1 = document.getElementById('atq1');
-btn1.addEventListener('click',function(){
-    attaque(btn1.getAttribute('name_atq'),btn1.getAttribute('power'),btn1.getAttribute('accuracy'),btn1.getAttribute('name_poke'),btn1.getAttribute('level'));
-})
 let btn2 = document.getElementById('atq2');
-btn2.addEventListener('click',function(){
-  attaque(btn2.getAttribute('name_atq'),btn2.getAttribute('power'),btn2.getAttribute('accuracy'),btn2.getAttribute('name_poke'),btn2.getAttribute('level'));
-})
 let btn3 = document.getElementById('atq3');
-btn3.addEventListener('click',function(){
-  attaque(btn3.getAttribute('name_atq'),btn3.getAttribute('power'),btn3.getAttribute('accuracy'),btn3.getAttribute('name_poke'),btn3.getAttribute('level'));
-})
 let btn4 = document.getElementById('atq4');
-btn4.addEventListener('click',function(){
-  attaque(btn4.getAttribute('name_atq'),btn4.getAttribute('power'),btn4.getAttribute('accuracy'),btn4.getAttribute('name_poke'),btn4.getAttribute('level'));
-})
+let atq_en = document.getElementById('attaques_ennemies').children;
+btn1.addEventListener('click',atq_click);
+btn2.addEventListener('click', atq_click);
+btn3.addEventListener('click',atq_click);
+btn4.addEventListener('click',atq_click);
+
+function atq_click(e){
+  let btn = e.target;
+  let resp;
+  let atq_rdm = atq_en[Math.floor(Math.random()*atq_en.length)]
+    attaque(btn.getAttribute('name_atq'),btn.getAttribute('power'),btn.getAttribute('accuracy'),btn.getAttribute('name_poke'),btn.getAttribute('level'));
+    btn1.removeEventListener('click',atq_click);   // on desactive les bouttons 
+    btn2.removeEventListener('click',atq_click);
+    btn3.removeEventListener('click',atq_click);
+    btn4.removeEventListener('click',atq_click);
+    setTimeout(function(){
+      btn1.addEventListener('click',atq_click);   // on desactive les bouttons 
+      btn2.addEventListener('click',atq_click);
+      btn3.addEventListener('click',atq_click);
+      btn4.addEventListener('click',atq_click);
+      resp = attaque_ennemies(atq_rdm.getAttribute('name_atq'),atq_rdm.getAttribute('power'),atq_rdm.getAttribute('accuracy'),atq_rdm.getAttribute('name_poke'),atq_rdm.getAttribute('level'));
+      if (resp){
+        btn1.removeEventListener('click',atq_click);   // on desactive les bouttons 
+        btn2.removeEventListener('click',atq_click);
+        btn3.removeEventListener('click',atq_click);
+        btn4.removeEventListener('click',atq_click);
+      }
+    },1500)
+   
+}
 
 document.getElementById('controls').style.display = 'none';
-function startBattle(rayquaza,charizard){
-    let turn = true;
-    while(rayquaza.hp > 0 || charizard.hp > 0){
-        // if(turn){
-        //     charizard.chooseRandomAtq();
-        // }
-        turn = switchBool(turn);
-    }
-}
-
-function switchBool(bool){
-    if (bool){
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
-// startBattle(rayquaza,charizard);
-
--- public/js/modules/pokemons.js
+++ public/js/modules/pokemons.js
@@ -1,88 +1,60 @@
-let narr_box = document.getElementById('narrative_box')
-// export let rayquaza = {
-//     name : 'rayquaza',
-//     hp: 100,
-//     vitesse: 97,
-//     Mâchouille(cible){
-//         let dmg = 15;
-//         let accuracy = 95;
-       
-//         if (Math.floor(Math.random()*100) >= accuracy){
-//             narr_box.innerHTML = `${this.name} rate son attaque, la precision de cette attaque est de ${accuracy} sur 100`;
-//         }
-//         else {
-//             narr_box.innerHTML = `${this.name} utilise machouille et fait ${dmg} de degats`;
-//             cible.hp -= dmg;
-//             document.getElementById('poke_life2').value -= dmg;
-//             document.getElementById('life_label2').innerHTML =  cible.hp
-//         }
-      
-        
-
-//     }
-// }
-
-
-// export let charizard = {
-//     name : 'charizard',
-//     hp:100,
-//     vitesse: 97,
-//     chooseRandomAtq(){
-//         return this.liste_atq[Math.floor(Math.random()*this.liste_atq.length)]
-//     },
-//     Machouille(cible){
-//         let dmg = 15;
-//         let accuracy = 95;
-       
-//         if (Math.floor(Math.random()*100) >= accuracy){
-//             narr_box.innerHTML = `${this.name} rate son attaque, la precision de cette attaque est de ${accuracy} sur 100`;
-//         }
-//         else {
-//             narr_box.innerHTML = `${this.name} utilise machouille et fait ${dmg}  de degats`;
-//             cible.hp -= dmg;
-//             document.getElementById('poke_life1').value -= dmg;
-//             document.getElementById('life_label1').innerHTML =  cible.hp
-//         }
-      
-        
-
-//     },
-//     Deflafration(cible){
-//         let dmg = 40;
-//         let accuracy = 60;
-       
-//         if (Math.floor(Math.random()*100) >= accuracy){
-//             narr_box.innerHTML = `${this.name} rate son attaque, la precision de cette attaque est de ${accuracy} sur 100`;
-//         }
-//         else {
-//             narr_box.innerHTML = `${this.name} utilise machouille et fait ${dmg}  de degats`;
-//             cible.hp -= dmg;
-//             document.getElementById('poke_life1').value -= dmg;
-//             document.getElementById('life_label1').innerHTML =  cible.hp
-//         }
-      
-        
-
-//     }
-// }
+export let narr_box = document.getElementById('narrative_box')
 
 export function attaque(name_atq,power,accuracy,name_poke,level){
     let dmg = Math.floor((((parseInt(level)*0.4+2)*2*power)/2*50)*1.2*0.5*1/1000);
-    
-    if (Math.floor(Math.random()*100) >= accuracy){
-        narr_box.innerHTML = `${name_poke} rate son attaque, la precision de cette attaque est de ${accuracy} sur 100`;
-    }
-    else {
-        narr_box.innerHTML = `${name_poke} utilise ${name_atq} et fait ${dmg} de degats`;
-        if(document.getElementById('poke_life2').value >= dmg){
-            document.getElementById('poke_life2').value -= dmg;
+    if(parseInt(document.getElementById('life_label2').innerHTML) > 0 ){
+        if (dmg){
+            if (Math.floor(Math.random()*100) >= accuracy){
+                narr_box.innerHTML = `${name_poke} rate son attaque, la precision de cette attaque est de ${accuracy} sur 100`;
+            }
+            else {
+                narr_box.innerHTML = `${name_poke} utilise ${name_atq} et fait ${dmg} de degats`;
+                if(document.getElementById('poke_life2').value >= dmg){
+                    document.getElementById('poke_life2').value -= dmg;
+                }
+                else{
+                    document.getElementById('poke_life2').value -=   document.getElementById('poke_life2').value;
+                }
+                document.getElementById('life_label2').innerHTML =   document.getElementById('poke_life2').value;
+        
+                if (parseInt(document.getElementById('life_label2').innerHTML) ==0){
+                    narr_box.innerHTML = `${document.getElementById('name_poke_2').innerHTML} est mort, ${name_poke} a gagne `;
+                    return true;
+                }
+            }
         }
         else{
-            document.getElementById('poke_life2').value -=   document.getElementById('poke_life2').value;
+            narr_box.innerHTML = 'Cette attaque ne fait techniquement pas de degats'
         }
-    
-       
-        document.getElementById('life_label2').innerHTML =   document.getElementById('poke_life2').value
     }
+
+          
+}
+
+export function attaque_ennemies(name_atq,power,accuracy,name_poke,level){
+    let dmg = Math.floor((((parseInt(level)*0.4+2)*2*power)/2*50)*1.2*0.5*1/1000);
+    if(dmg){
+        if (Math.floor(Math.random()*100) >= accuracy){
+            narr_box.innerHTML = `${name_poke} rate son attaque, la precision de cette attaque est de ${accuracy} sur 100`;
+        }
+        else {
+            narr_box.innerHTML = `${name_poke} utilise ${name_atq} et fait ${dmg} de degats`;
+            if(document.getElementById('poke_life1').value >= dmg){
+                document.getElementById('poke_life1').value -= dmg;
+            }
+            else{
+                document.getElementById('poke_life1').value -=   document.getElementById('poke_life1').value;
+            }
+            document.getElementById('life_label1').innerHTML =   document.getElementById('poke_life1').value;
+            if (parseInt(document.getElementById('life_label1').innerHTML) ==0){
+                narr_box.innerHTML = `${document.getElementById('name_poke_1').innerHTML} est mort, ${name_poke} a gagne `;
+                return true;
+            }
+        }
+    }
+    else{
+        narr_box.innerHTML = `${name_poke} utilise ${name_atq} mais cette attaque ne fait techniquement pas de degats`
+    }
+
           
 }
